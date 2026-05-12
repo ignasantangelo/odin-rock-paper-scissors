afeee4ff47658621f285d8ea48d2eaf74b8504f1
Ignacio <ignaciosantangelo25@gmail.com>
2026-05-12 01:54:58 +0000

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper';
+        case 3:
+            return 'Scissors';
+    }
+}
